1186ab063ffe5270a8deb9dd9b02b9d6c50ea238
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-10-02 21:07:21 +0000

    next set of experiments

--- sample_train.py
+++ sample_train.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8x.yaml")
-model = YOLO("yolov8x.pt")
-model = YOLO("yolov8x.yaml").load("yolov8x.pt")
+model = YOLO("yolov10x.yaml")
+model = YOLO("yolov10x.pt")
+model = YOLO("yolov10x.yaml").load("yolov10x.pt")
 
 # Train the model
 results = model.train(data="minedata.yaml", epochs=100, imgsz=640, 
-                      project="mining_object_detection", name="experiement")
+                      project="mining_object_detection", name="yolov10")
